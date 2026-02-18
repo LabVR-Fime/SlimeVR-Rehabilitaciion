6a36bf7bf3cb5ac3c860f8b6fc5a2138f08f4674
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2026-02-18 16:50:10 -0600

    Añadido: Animación de contexto (Controles)
    Se hizo una animación completa con 3 íconos de indicaciones que desaparece.
    Se añadieron los recursos necesarios en Assets para replicar los íconos en otras escenas.

--- Assets/Scripts/UI_Animaciones.cs
+++ Assets/Scripts/UI_Animaciones.cs
@@ -1,16 +1,44 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class UI_Animaciones : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject GrupoCanvas;
+
+    [SerializeField] private GameObject ContextoControl_Mouse;
+    [SerializeField] private GameObject ContextoControl_Teclado;
+    [SerializeField] private GameObject ContextoControl_Cuerpo;
+
+    [SerializeField] public RawImage IMG_SVEI1;
+    [SerializeField] public RawImage IMG_SVEI2;
+    [SerializeField] public RawImage IMG_SVEI3;
+
+
+    private void Start()
     {
-        
+ 
+        //Esto mueve los dibujitos al centro pero de manera "rebotona"
+        LeanTween.moveX(ContextoControl_Mouse.GetComponent<RectTransform>(), -480, 1.5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBounce);
+        LeanTween.moveX(ContextoControl_Teclado.GetComponent<RectTransform>(), 20, 1.5f).setDelay(1.8f).setEase(LeanTweenType.easeOutBounce);
+        LeanTween.moveX(ContextoControl_Cuerpo.GetComponent<RectTransform>(), 520, 0.8f).setDelay(2.0f).setEase(LeanTweenType.easeOutBounce);
+
+        //Esto mueve los textos al centro pero de manera "rebotona"
+        LeanTween.moveX(IMG_SVEI1.GetComponent<RectTransform>(), 520, 1.5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBounce);   //Cuerpo
+        LeanTween.moveX(IMG_SVEI2.GetComponent<RectTransform>(), -480, 1.5f).setDelay(1.8f).setEase(LeanTweenType.easeOutBounce);     //Mouse
+        LeanTween.moveX(IMG_SVEI3.GetComponent<RectTransform>(), 20, 0.8f).setDelay(2.0f).setEase(LeanTweenType.easeOutBounce);    //Teclado
+        BajarAlpha();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void BajarAlpha()
     {
-        
+        LeanTween.alpha(GrupoCanvas.GetComponent<RectTransform>(), 0f, 4f).setDelay(8.0f);
+        GrupoCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+
     }
 }
