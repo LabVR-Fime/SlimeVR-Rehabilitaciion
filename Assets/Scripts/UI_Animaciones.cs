bb69dba6e2b558f2c12ae9ed83f09bd49d6e1aea
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2026-02-18 20:20:01 -0600

    Antes de irse
    Fue lo que dejaste

--- Assets/Scripts/UI_Animaciones.cs
+++ Assets/Scripts/UI_Animaciones.cs
@@ -18,12 +18,10 @@ public class UI_Animaciones : MonoBehaviour
     [SerializeField] public RawImage IMG_SVEI3;
 
     [SerializeField] public TMPro.TMP_Text TextoMoverX;
-    private RectTransform rt;
+
 
     private void Start()
     {
-        rt = TextoMoverX.GetComponent<RectTransform>();
-
         //Esto mueve los dibujitos al centro pero de manera "rebotona"
         LeanTween.moveX(ContextoControl_Mouse.GetComponent<RectTransform>(), -480, 1.5f).setDelay(1.5f).setEase(LeanTweenType.easeOutBounce);
         LeanTween.moveX(ContextoControl_Teclado.GetComponent<RectTransform>(), 20, 1.5f).setDelay(1.8f).setEase(LeanTweenType.easeOutBounce);
@@ -44,17 +42,6 @@ public class UI_Animaciones : MonoBehaviour
 
 
     }
-
-    public void AgitarX()
-    {
-        float startX = rt.anchoredPosition.x;
-        float offset = 10f;
-        LeanTween.moveX(rt, startX + offset, 0.1f)
-            .setEase(LeanTweenType.easeOutQuad)
-            .setOnComplete(() =>
-            {
-                LeanTween.moveX(rt, startX, 0.3f)
-                    .setEase(LeanTweenType.easeOutBounce);
-            });
-    }
 }
+
+
