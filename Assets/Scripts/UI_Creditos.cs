c7d00279d3cba37f18e43b1145e38886a137a12b
Ranfery <122646497+LabVR-Fime@users.noreply.github.com>
2026-05-28 20:45:08 -0600

    Añadidos: Botón de volver
    No funciona, hay que ponerle correctamente el Script de volver al menú.

--- Assets/Scripts/UI_Creditos.cs
+++ Assets/Scripts/UI_Creditos.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class UI_Creditos : MonoBehaviour
 {
+
+    public TMP_Text Creditos;
+
     void Start()
     {
-        RectTransform rt = TextoCreditos.GetComponent<RectTransform>();         //Declaramos la variable de créditos.
-        float InicioY = rt.anchoredPosition.y;                                  //Declaramos la posición inicial
+        RectTransform rt = Creditos.GetComponent<RectTransform>();         //Declaramos la variable de crï¿½ditos.
+        float InicioY = rt.anchoredPosition.y;                                  //Declaramos la posiciï¿½n inicial
 
         LeanTween.moveY(rt, InicioY + 3000, 40f)             //Mover algo 3000 unidades hacia arriba a 40uph
             .setEaseLinear();                                //De manera lineal
