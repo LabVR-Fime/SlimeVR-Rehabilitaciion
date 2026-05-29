a4aaf7eeecf56bc6ebfbc4bde59330b77baf2bea
Ranfery <122646497+LabVR-Fime@users.noreply.github.com>
2026-05-29 16:08:38 -0600

    Añadida: Animación para los créditos
    Al abrir la escena de créditos, todos los créditos se mueven hacia arriba despacito.

--- Assets/Scripts/UI_Creditos.cs
+++ Assets/Scripts/UI_Creditos.cs
@@ -4,14 +4,14 @@ using TMPro;
 public class UI_Creditos : MonoBehaviour
 {
 
-    public TMP_Text Creditos;
+    public TMP_Text Creditos;       //Nombres de créditos
 
     void Start()
     {
-        RectTransform rt = Creditos.GetComponent<RectTransform>();         //Declaramos la variable de cr�ditos.
-        float InicioY = rt.anchoredPosition.y;                                  //Declaramos la posici�n inicial
+        RectTransform rt = Creditos.GetComponent<RectTransform>();      //Declaramos la variable de cr�ditos.
+        float InicioY = rt.anchoredPosition.y;                          //Declaramos la posici�n inicial
 
-        LeanTween.moveY(rt, InicioY + 3000, 40f)             //Mover algo 3000 unidades hacia arriba a 40uph
+        LeanTween.moveY(rt, InicioY + 80000, 1070)           //Mover algo 80000 unidades hacia arriba a 1070uph
             .setEaseLinear();                                //De manera lineal
             }
 
